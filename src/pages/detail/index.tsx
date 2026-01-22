797a4cb2e6b82ff152b7b8673e6051d65b331bd3
Felipe Tajima <felipebtajima05@gmail.com>
2026-01-22 19:55:15 -0300

    Adicionando Rotas

---
+++ src/pages/detail/index.tsx
@@ -0,0 +1,10 @@
+function Detail() {
+
+  return (
+    <div>
+        <h1>Detalhe da moeda</h1>
+    </div>
+  )
+}
+
+export default Detail
---
+++ src/pages/notfound/index.tsx
@@ -0,0 +1,10 @@
+function Notfound() {
+
+  return (
+    <div>
+        <h1>ERROR 404: PAGE DOESN'T EXISTS</h1>
+    </div>
+  )
+}
+
+export default Notfound
